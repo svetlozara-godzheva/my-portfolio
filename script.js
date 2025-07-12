0542fd1889811d29c552f6702224ad9e30c196ab
svetlozara-godzheva <n01754024@humber.ca>
2025-07-11 23:25:38 -0400

    Implemented the contact form

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const projectDetailsButtons = document.querySelectorAll(".project-details-btn");
 
 projectDetailsButtons.forEach(button => {
-    button.addEventListener('click', function (event) {
+    button.addEventListener("click", function (event) {
         /**
         * @type {HTMLButtonElement}
         */
@@ -20,3 +20,62 @@ projectDetailsButtons.forEach(button => {
         }
     });
 });
+
+const submitButton = document.getElementById("submit-btn");
+submitButton.addEventListener("click", function (e) {
+    checkName(e);
+    checkEmail(e);
+    checkMessage(e);
+});
+
+function checkName(e) {
+    const nameInput = document.getElementById("name");
+    /**
+   * @type {string} 
+   */
+    const nameValue = nameInput.value;
+
+    const messageElement = document.getElementById("name-error");
+
+    if (nameValue.trim() === "") {
+        messageElement.innerHTML = "You should enter your name"
+        e.preventDefault();
+    } else {
+        messageElement.innerHTML = "";
+    }
+}
+
+function checkEmail(e) {
+    const emailInput = document.getElementById("email");
+    /**
+   * @type {string} 
+   */
+    const emailValue = emailInput.value;
+
+    const messageElement = document.getElementById("email-error");
+
+    if (emailValue.trim() === "") {
+        messageElement.innerHTML = "You should enter an email"
+        e.preventDefault();
+    } else {
+        messageElement.innerHTML = "";
+    }
+}
+
+function checkMessage(e) {
+    const messageInput = document.getElementById("message");
+    /**
+   * @type {string} 
+   */
+    const messageValue = messageInput.value;
+
+    const messageElement = document.getElementById("message-error");
+
+    if (messageValue.trim() === "") {
+        messageElement.innerHTML = "You should enter a message";
+        e.preventDefault();
+    } else {
+        messageElement.innerHTML = "";
+    }
+}
+
